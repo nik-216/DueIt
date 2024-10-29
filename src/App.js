13dd996d39fc548368b24c17022a62b1dc3167f9
Nikitha <nik-216@users.noreply.github.com>
2024-10-29 14:25:21 +0530

    trial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import react from 'react';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>hi</p>
     </div>
   );
 }
